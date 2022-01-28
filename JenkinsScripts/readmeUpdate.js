08b7b82e961ce4425340e89a2ed2df10be8c50ef
RTPablocs <conejerosorianopablo@gmail.com>
2022-01-28 16:26:57 +0100

    added JenkinsScripts

--- JenkinsScripts/readmeUpdate.js
+++ JenkinsScripts/readmeUpdate.js
@@ -5,6 +5,8 @@ const testResult = process.argv[2]
 const successBadge = 'https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg'
 const failedBadge = 'https://img.shields.io/badge/test-failure-red.svg'
 
+console.log(testResult)
+
 fs.readFile('README.md', 'utf-8', (err,data) => {
     if (err) {
         throw err
